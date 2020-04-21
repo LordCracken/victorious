ec8ee31c37a70346c22dd1a2774a83efd0ddacd7
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-04-21 14:38:29 +0300

    feat: add transition effect cost section list

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.cost__more').on('click', () => {
-    $('.cost-items').toggleClass('cost-items--extended');
+    $('.cost-item--interactive').toggleClass('cost-items__item--hidden');
     $('.cost__more').toggleClass('cost__more--active');
     $('.cost__text').toggleClass('cost__text--min-mod');
   });
