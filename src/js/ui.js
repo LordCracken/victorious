635155ebbd22a9a4498d2c77d8c8aed805686169
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-04-24 18:04:12 +0300

    feat: add questions section

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
+  // Кнопка "Больше" в секции "Что входит в стоимость"
   $('.cost__more').on('click', () => {
     $('.cost-item--interactive').toggleClass('cost-items__item--hidden');
     $('.cost__more').toggleClass('cost__more--active');
     $('.cost__text').toggleClass('cost__text--min-mod');
+  });
+  // Открытие и закрытие вопросов
+  $('.questions__question').on('click', () => {
+    $(event.target).next('.questions__answer').toggleClass('questions__answer--opened');
   });
 });
