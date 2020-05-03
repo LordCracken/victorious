7171f487e18e55e4f2245575ae489a7e660f33f0
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-05-03 10:00:25 +0300

    feat: add searching advance and contacts sections

---
+++ src/js/map.js
@@ -0,0 +1,32 @@
+ymaps.ready(function () {
+  const myMap = new ymaps.Map('map', {
+      center: [55.57480050410017,37.617334500000005],
+      zoom: 17
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Метка',
+      balloonContent: 'Метка'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/map-token.png',
+      // Размеры метки.
+      iconImageSize: [19, 31],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+    });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+});
