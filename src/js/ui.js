5d53834a46c59b2f91dfeaad21ebb1a8c651f441
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-05-06 18:37:33 +0300

    feat: add modal windows

--- src/js/ui.js
+++ src/js/ui.js
@@ -20,4 +20,47 @@ $(document).ready(function () {
     $('.nav').removeClass('nav--active');
     $('.menu-button').removeClass('menu-button--active');
   });
+  // Модальные окна
+  $('.about-us__button').on('click', () => {
+    $('.modal-block').removeClass('modal-block--hidden');
+    $('.project-application-modal').removeClass('project-application-modal--hidden');
+  });
+  $('.project-application-modal__close-modal').on('click', () => {
+    $('.modal-block').addClass('modal-block--hidden');
+    $('.project-application-modal').addClass('project-application-modal--hidden');
+  });
+
+  $('.our-offer__button').on('click', () => {
+    $('.modal-block').removeClass('modal-block--hidden');
+    $('.business-proposal-modal').removeClass('business-proposal-modal--hidden');
+  });
+  $('.business-proposal-modal__close-modal').on('click', () => {
+    $('.modal-block').addClass('modal-block--hidden');
+    $('.business-proposal-modal').addClass('business-proposal-modal--hidden');
+  });
+
+  $('.rate__button').on('click', () => {
+    $('.modal-block').removeClass('modal-block--hidden');
+    $('.everymonth-works-modal').removeClass('everymonth-works-modal--hidden');
+  });
+  $('.everymonth-works-modal__close-modal').on('click', () => {
+    $('.modal-block').addClass('modal-block--hidden');
+    $('.everymonth-works-modal').addClass('everymonth-works-modal--hidden');
+  });
+  $(document).keyup(function (e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      $('.modal-block').addClass('modal-block--hidden');
+      $('.everymonth-works-modal').addClass('everymonth-works-modal--hidden');
+      $('.business-proposal-modal').addClass('business-proposal-modal--hidden');
+      $('.project-application-modal').addClass('project-application-modal--hidden');
+    }
+  });
+  $('.modal-block').on('click', (e) => {
+    if (!$('.modal').is(e.target) && $('.modal').has(e.target).length === 0) {
+      $('.modal-block').addClass('modal-block--hidden');
+      $('.everymonth-works-modal').addClass('everymonth-works-modal--hidden');
+      $('.business-proposal-modal').addClass('business-proposal-modal--hidden');
+      $('.project-application-modal').addClass('project-application-modal--hidden');
+    }
+  });
 });
